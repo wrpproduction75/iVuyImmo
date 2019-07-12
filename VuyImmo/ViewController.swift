b355745d6bdafdd0c9bea32230688715b4a2186b
jlheriniaina <jlheriniaina@gmail.com>
2019-07-12 21:32:05 +0300

    Initial Commit

---
+++ VuyImmo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VuyImmo
+//
+//  Created by Lucas on 12/07/2019.
+//  Copyright © 2019 Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
